4130f2504a10f56a9b441ec95ebe4a7a3451dd5e
Geoffrey Gourlez <geoffrey.gourlez@axa.fr>
2019-07-23 10:59:50 +0200

    feat(exo03): Add assert for exercice 03

--- es6/03_promiseAll.mjs
+++ es6/03_promiseAll.mjs
@@ -1,20 +1,11 @@
-import { callPromise } from './00_utils.mjs';
+import { callPromise, assert } from './00_utils.mjs';
 
 let p1 = callPromise("100mil", 100);
 let p2 = callPromise("1s", 1000);
 let p3 = callPromise("2s", 2000);
 let p4 = callPromise("3s", 3000);
 
-Promise.all([p1, p2, p3, p4]).then(values => {
-  console.log("All promises are finished");
-  console.log(values);
+Promise.all([p1, p2, p3, p4]).then(values => {  
+  let expectedResult = null; // YOUR RESPONSE HERE => Replace null by the returned object;
+  assert(JSON.stringify(expectedResult) == JSON.stringify(values), "all promise");
 });
-
-// console result
-// > All promises are finished
-// > [
-// >   'Promise 100mil resolved in 100',
-// >   'Promise 1s resolved in 1000',
-// >   'Promise 2s resolved in 2000',
-// >   'Promise 3s resolved in 3000'
-// > ]
