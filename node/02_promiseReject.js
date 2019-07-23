3e2f857af240b2df456eadca233aa85264e538ca
Geoffrey Gourlez <geoffrey.gourlez@axa.fr>
2019-07-23 07:32:09 +0200

    doc(exo02): Add documentation for exo 02

--- node/02_promiseReject.js
+++ node/02_promiseReject.js
@@ -2,8 +2,12 @@
 
 const callPromise = require('./00_utils.js').callPromise;
 
+// In the function callPromise, the Boolean is herre to set the promise in error.
+// if it's true, we called the reject function.
+// When the reject function is called the promise is in error.
 let promise = callPromise("In Error", 2000, true); //set promise in error
 
+// So, we have to use the catch method when a promise is in error.
 promise.catch(error => console.log(error));
 
 // console result 
