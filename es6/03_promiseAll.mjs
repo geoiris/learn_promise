92c118a85aff7ea9a13d54a53d1d969ba947cfb1
Geoffrey Gourlez <geoffrey.gourlez@axa.fr>
2019-07-24 08:17:40 +0200

    docs(exo03): Add documentation for exo 03

--- es6/03_promiseAll.mjs
+++ es6/03_promiseAll.mjs
@@ -5,6 +5,9 @@ let p2 = callPromise("1s", 1000);
 let p3 = callPromise("2s", 2000);
 let p4 = callPromise("3s", 3000);
 
+// In this case, all promises are resolved. But not in the same time.
+// So, when we want to wait that all promises are resolve before make traitement
+// we have to use Promise.All()
 Promise.all([p1, p2, p3, p4]).then(values => {
   console.log("All promises are finished");
   console.log(values);
