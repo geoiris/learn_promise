aeb30df5e59610fbdb67885533a3e3d0ea3e118e
Geoffrey Gourlez <geoffrey.gourlez@axa.fr>
2019-07-24 08:20:28 +0200

    docs(exos03): Add instructions for exo 03

--- es6/03_promiseAll.mjs
+++ es6/03_promiseAll.mjs
@@ -5,6 +5,7 @@ let p2 = callPromise("1s", 1000);
 let p3 = callPromise("2s", 2000);
 let p4 = callPromise("3s", 3000);
 
+// In this exercice we want ti wait that all promises are resolved
 Promise.all([p1, p2, p3, p4]).then(values => {  
   let expectedResult = null; // YOUR RESPONSE HERE => Replace null by the returned object;
   assert(JSON.stringify(expectedResult) == JSON.stringify(values), "all promise");
