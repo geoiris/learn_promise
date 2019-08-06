dab19f6ca74c428a4b45fed2dba0d1390d410699
Geoffrey Gourlez <geoffrey.gourlez@axa.fr>
2019-08-06 10:09:58 +0200

    doc(exo04): Add documentation for exo 04

--- es6/04_promiseAllReject.mjs
+++ es6/04_promiseAllReject.mjs
@@ -2,9 +2,13 @@ import { callPromise } from "./00_utils.mjs";
 
 let p1 = callPromise("100mil", 100);
 let p2 = callPromise("1s", 1000);
-let p3 = callPromise("errorPromise", 1000, true);
+let p3 = callPromise("errorPromise", 1000, true); // We set the promise in error
 let p4 = callPromise("3s", 3000);
 
+// In this case, one promise are in error. So with Promise.All if one promise are in error,
+// the result is in error.
+// We passed in the catch.
+// There is one way to write the catch :
 Promise.all([p1, p2, p3, p4])
   .then(values => {
     // Because failure, this code is never called
@@ -16,7 +20,7 @@ Promise.all([p1, p2, p3, p4])
     console.log(err);
   });
 
-// Other way to write catch method
+// Other way to write catch method, if Promise.All are in error, the function take an error paremeter :
 Promise.all([p1, p2, p3, p4]).then(
   values => {
     // Because failure, this code is never called
