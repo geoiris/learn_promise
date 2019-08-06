3554fa480b14b4013ec792593f1639667b2966fa
Geoffrey Gourlez <geoffrey.gourlez@axa.fr>
2019-08-06 10:13:35 +0200

    doc(exo05): Add documentation for NodeJs exo 05

--- node/05_promiseRace.js
+++ node/05_promiseRace.js
@@ -7,6 +7,8 @@ let p2 = callPromise("Two", 1000);
 let p3 = callPromise("Three", 100);
 let p4 = callPromise("Four", 3000);
 
+// The Promise Race return the result of the first terminated promise
+// Here, the promise Three is faster
 Promise.race([p1,p2,p3,p4]).then(value => {
   console.log(value);
 });
